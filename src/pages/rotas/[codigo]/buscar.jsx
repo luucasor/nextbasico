f3e587b3f2501829f30084442930448eb1998f28
luucasor <lucas@jornadatecnologia.com>
2023-04-08 11:27:08 -0300

    Exemplo 2 rotas dinâmicas, com o uso de parâmetros na url

---
+++ src/pages/rotas/[codigo]/buscar.jsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+export default function buscar() {
+    const router = useRouter()
+    const codigo = router.query.codigo
+    return (
+        <div>
+            <h1>Rotas/{codigo}/Buscar!!!</h1>
+        </div>
+    )
+}
